1605e2d46c4054deccf460047ef89ecc13501d81
Alexey Grevtsov <grevold@yandex.ru>
2026-04-19 23:11:22 +0300

    добавил блок клиенты

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "./styles/global.css";
 import { Capability } from "./components/capability/capability";
+import { Clients } from "./components/clients/clients";
 import { Footer } from "./components/footer/footer";
 import { Header } from "./components/header/header";
 import { Nav } from "./components/nav/nav";
@@ -9,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const header = new Header();
   header.getElement();
   new Capability();
+  new Clients();
   const footer = new Footer();
   footer.getElement();
 });
