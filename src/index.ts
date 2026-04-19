575e88a7e117baa22a82577982b25af112ca30c9
Alexey Grevtsov <grevold@yandex.ru>
2026-04-19 21:26:58 +0300

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "./styles/global.css";
 import { Capability } from "./components/capability/capability";
 import { Footer } from "./components/footer/footer";
 import { Header } from "./components/header/header";
+import { Nav } from "./components/nav/nav";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Nav();
