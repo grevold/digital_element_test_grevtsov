ab36739be27359dd0dd9a1c773fd9b0bb8525487
Алексей <87669945+grevold@users.noreply.github.com>
2026-04-19 21:19:46 +0300

    Capability (#2)
    * добавил модалку, хедер, футер
    
    * add capability

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import "./styles/global.css";
+import { Capability } from "./components/capability/capability";
 import { Footer } from "./components/footer/footer";
 import { Header } from "./components/header/header";
 
 document.addEventListener("DOMContentLoaded", () => {
+  new Nav();
   const header = new Header();
   header.getElement();
+  new Capability();
   const footer = new Footer();
   footer.getElement();
 });
